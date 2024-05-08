c90e72a951351899107d8c4b56ab69119b9dc25c
Gomssy <gggomksy@naver.com>
2024-05-08 20:43:02 +0900

    Init Board

---
+++ Assets/Scripts/BoardManager.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardManager : Singleton<BoardManager>
+{
+    public static int max_x { get; set; } = 10;
+    public static int max_y { get; set; } = 5;
+
+    public Board boardPrefab;
+    public List<Board> boards = new List<Board>();
+
+    private void Awake()
+    {
+        InitBoard();
+    }
+
+    private void InitBoard()
+    {
+        for(int x = 0; x < max_x; x++)
+        {
+            for(int y = 0; y < max_y; y++)
+            {
+                var board = CreateBoard(x, y);
+                boards.Add(board);
+            }
+        }
+    }
+
+    private Board CreateBoard(int x, int y)
+    {
+        var board = Instantiate(boardPrefab);
+        board._x = x; board._y = y;
+        board.transform.SetParent(transform, false);
+        board.transform.position = new Vector3(x,y,0f);
+        board.name = new Vector2Int(x, y).ToString();
+
+        return board;
+    }
+
+}
